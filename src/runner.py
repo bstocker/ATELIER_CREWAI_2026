9ce55c028de8baa44a3c6018868ed29b8dbce952
bstocker <boris.stocker@gmail.com>
2026-04-23 12:23:57 +0200

    Update runner.py

--- src/runner.py
+++ src/runner.py
@@ -44,12 +44,12 @@ MODULE_CONFIG = {
         "output_dir": BASE_DIR / "modules" / "bloc" / "outputs",
         "agents_file": CONFIG_DIR / "agents_bloc.yaml",
         "tasks_file": CONFIG_DIR / "tasks_bloc.yaml",
-        "formal_agent_key": "formal_bloc_analyst",
-        "real_agent_key": "real_bloc_analyst",
-        "alignment_agent_key": "alignment_bloc_analyst",
-        "formal_task_key": "formal_bloc_task",
-        "real_task_key": "real_bloc_task",
-        "alignment_task_key": "alignment_bloc_task",
+        "formal_agent_key": "formal_analyst",
+        "real_agent_key": "real_analyst",
+        "alignment_agent_key": "alignment_analyst",
+        "formal_task_key": "formal_task",
+        "real_task_key": "real_task",
+        "alignment_task_key": "alignment_task",
     },
 }
 
